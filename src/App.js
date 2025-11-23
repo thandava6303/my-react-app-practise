c8ba4afa9cc5f324e2d8f2d923816140ba2dd93c
thandava6303 <80169246+thandava6303@users.noreply.github.com>
2025-11-23 11:39:59 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>Hello from Jenkins CI/CD 🚀</h1>
+      <p>React App Deployment Successful!</p>
+    </div>
+  );
+}
+
+export default App;
